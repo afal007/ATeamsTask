46b3bc0a44360564ebad3c0d73fd1ee278c78bee
afal <falalexandr007@gmail.com>
2017-10-09 13:43:26 +0700

    Added retrolambda support

---
+++ app/src/main/java/io/github/afal007/ateamstask/controllers/activities/MainActivity.java
@@ -0,0 +1,36 @@
+package io.github.afal007.ateamstask.controllers.activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import io.github.afal007.ateamstask.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_contacts:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        });
+    }
+
+}
