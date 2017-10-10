a9d8bfe9093725fe759dddd4cf863cf70b4908d8
afal <falalexandr007@gmail.com>
2017-10-10 17:12:11 +0700

    Small fixes
    Changed some id names.
    Added windowSoftInputMode="adjustPan".

--- app/src/main/java/io/github/afal007/ateamstask/mvcviews/main/MainViewMvc.java
+++ app/src/main/java/io/github/afal007/ateamstask/mvcviews/main/MainViewMvc.java
@@ -3,14 +3,12 @@ package io.github.afal007.ateamstask.mvcviews.main;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.NavigationView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.github.afal007.ateamstask.R;
-import io.github.afal007.ateamstask.controllers.activities.MainActivity;
 import io.github.afal007.ateamstask.mvcviews.AbstractViewMvc;
 
 /**
@@ -19,7 +17,7 @@ import io.github.afal007.ateamstask.mvcviews.AbstractViewMvc;
 
 public class MainViewMvc extends AbstractViewMvc {
 
-    @BindView(R.id.navigation) BottomNavigationView mBottomNav;
+    @BindView(R.id.main_bottom_navigation) BottomNavigationView mBottomNav;
 
     public MainViewMvc(@NonNull LayoutInflater layoutInflater,
                        @Nullable ViewGroup container) {
@@ -29,5 +27,9 @@ public class MainViewMvc extends AbstractViewMvc {
 
     public void setOnNavItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener listener) {
         mBottomNav.setOnNavigationItemSelectedListener(listener);
+    }
+
+    public int getFragmentContainerId() {
+        return R.id.main_container;
     }
 }
